27db8d06ab80ec1e966e14b8fd27deb2d81c37ec
Colin Pelkmans <colin.pelkmans@gmail.com>
2018-05-01 23:01:37 +0200

    added random data generator

--- biometric-station-javafx-merge/src/biometric/station/javafx/merge/FXMLDocumentController.java
+++ biometric-station-javafx-merge/src/biometric/station/javafx/merge/FXMLDocumentController.java
@@ -6,30 +6,94 @@
 package biometric.station.javafx.merge;
 
 import java.net.URL;
+import java.util.Random;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
+import javafx.scene.control.Button;
 
 /**
  *
- * @author yoshi
+ * @author colin
  */
 public class FXMLDocumentController implements Initializable {
     
-    @FXML
-    private Label label;
+    @FXML private LineChart heartbeatSensorChart;
+    @FXML private LineChart accelorometerChart;
+    @FXML private LineChart temperatureChart;
+    @FXML private LineChart fingerprintSensorChart;
+    @FXML private Button generateData;
     
-    @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
+    private XYChart.Series heartbeatValues;
+    private XYChart.Series accelorometerValues;
+    private XYChart.Series temperatureValues;
+    
+    final NumberAxis xAxisHeart = new NumberAxis();
+    final NumberAxis yAxisHeart = new NumberAxis();
+    
+    final NumberAxis xAxisTemp = new NumberAxis();
+    final NumberAxis yAxisTemp = new NumberAxis();
+    
+    final NumberAxis xAxisAccel = new NumberAxis();
+    final NumberAxis yAxisAccel = new NumberAxis();
+    
+    private Random dataGenerator = new Random();
+    
+    private final int MAXIMUM_HEART = 200;
+    private final int MINIMUM_HEART = 50; 
+    private final int MAXIMUM_ACCEL = 50;
+    private final int MINIMUM_ACCEL = 0;
+    private final int MAXIMUM_TEMPERATURE = 50;
+    private final int MINIMUM_TEMPERATURE = 0;
+    
+    private int xValueHeart = 0;
+    private int xValueAccel = 0;
+    private int xValueTemp = 0;
+    
+     @FXML
+     private void generateRandomDataHandler(ActionEvent event) {
+        
+        double randomHeartbeat = dataGenerator.nextDouble()
+        * (MAXIMUM_HEART - MINIMUM_HEART + 1) + MINIMUM_HEART;
+        System.out.println(randomHeartbeat);
+        heartbeatValues.getData().add(new XYChart.Data(xAxisHeart, randomHeartbeat));
+        
+        double randomAccelerometer = dataGenerator.nextDouble()
+        * (MAXIMUM_ACCEL - MINIMUM_ACCEL + 1) + MINIMUM_ACCEL;
+        accelorometerValues.getData().add(new XYChart.Data(xAxisAccel, randomAccelerometer));
+        
+        double randomTemperature = dataGenerator.nextDouble()
+        * (MAXIMUM_TEMPERATURE - MINIMUM_TEMPERATURE + 1) + MINIMUM_TEMPERATURE;
+        temperatureValues.getData().add(new XYChart.Data(xAxisTemp, randomTemperature));
     }
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        heartbeatValues = new XYChart.Series();
+        heartbeatValues.setName("heartbeat in BPM");
+        heartbeatSensorChart.getData().add(heartbeatValues);
+        
+        accelorometerValues = new XYChart.Series();
+        accelorometerValues.setName("speed is m/s");
+        accelorometerChart.getData().add(accelorometerValues);
+        
+        temperatureValues = new XYChart.Series();
+        temperatureValues.setName("temperature in °C");
+        temperatureChart.getData().add(temperatureValues);
+
+       // Set Axis
+       heartbeatSensorChart.getYAxis().setLabel("Heartbeat");
+       heartbeatSensorChart.getXAxis().setLabel("Measurement");
+       
+       accelorometerChart.getYAxis().setLabel("Speed");
+       accelorometerChart.getXAxis().setLabel("Measurement");
+       
+       temperatureChart.getYAxis().setLabel("Temperature");
+       temperatureChart.getXAxis().setLabel("Measurement");
     }    
     
 }
